5ea73605017e3e5b4f8cf347432f37ee05927011
David Crawford <dcrawford@David-MacBook.local>
2022-12-03 14:26:12 -0500

    Added project

---
+++ CameraEmulator/CameraEmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraEmulatorApp.swift
+//  CameraEmulator
+//
+//  Created by David Crawford on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraEmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
